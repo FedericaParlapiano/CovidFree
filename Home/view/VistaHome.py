25a51a6f6a04c72b58884405d19c6b9b3064155b
AriannaRonci <s1093926@studenti.univpm.it>
2021-05-09 19:43:12 +0200

    vista home

---
+++ Home/view/VistaHome.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+
+
+class VistaHome(QWidget):
+
+    def __init__(self, parent=None):
+        super(VistaHome, self).__init__(parent)
+        grid_layout = QGridLayout()
+
+        grid_layout.addWidget(self.get_generic_button("Calendario Vaccini", "rgb(204,255,255)", self.go_calendario_vaccini), 0, 0)
+        grid_layout.addWidget(self.get_generic_button("Calendario Tamponi", "rgb(255,255,153)", self.go_calendario_tamponi), 1, 0)
+        grid_layout.addWidget(self.get_generic_button("Magazzino", "rgb(255,204,255)", self.go_magazzino), 2, 0)
+
+
+        self.setLayout(grid_layout)
+        self.resize(400, 300)
+        self.setWindowTitle("Clinica COVID FREE")
+
+    def get_generic_button(self, titolo, colore, on_click):
+        button = QPushButton(titolo)
+        button.setStyleSheet("background-color: {}".format(colore))
+        button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        button.clicked.connect(on_click)
+        return button
+
+    def go_calendario_vaccini(self):
+        pass
+
+    def go_calendario_tamponi(self):
+        pass
+
+    def go_magazzino(self):
+        pass
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import QApplication
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+from Home.view.VistaHome import VistaHome
 
 if __name__ == '__main__':
-    print_hi('a tutti quanti!')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    vista_home = VistaHome()
+    vista_home.show()
+    sys.exit(app.exec())
