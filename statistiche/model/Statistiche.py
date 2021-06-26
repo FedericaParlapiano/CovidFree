d41066ce921037acca2b83ae7fc604322820586f
FrancescaPalazzetti <S1094008@studenti.univpm.it>
2021-06-26 16:06:52 +0200

    Pie chart

--- statistiche/model/Statistiche.py
+++ statistiche/model/Statistiche.py
@@ -7,8 +7,8 @@ class Statistiche():
         super(Statistiche, self).__init__()
         self.elenco_appuntamenti_vaccini = []
         self.elenco_appuntamenti_tamponi = []
-        self.vaccino_per_tipologia = {"Astrazeneca": 0, "Moderna": 0, "Pfizer": 0}
-        self.tampone_per_tipologia = {"Antigenico Rapido": 0, "Molecolare": 0, "Sierologico": 0}
+
+        #self.tampone_per_tipologia = {"Antigenico Rapido": 0, "Molecolare": 0, "Sierologico": 0}
 
         if os.path.isfile('calendariovaccini/data/elenco_appuntamenti_fissati.pickle'):
             with open('calendariovaccini/data/elenco_appuntamenti_fissati.pickle', 'rb') as f:
@@ -17,16 +17,6 @@ class Statistiche():
         if os.path.isfile('calendariotamponi/data/elenco_appuntamenti_salvati.pickle'):
             with open('calendariotamponi/data/elenco_appuntamenti_salvati.pickle', 'rb') as f:
                 self.elenco_appuntamenti_tamponi = pickle.load(f)
-
-        for appuntamento in self.elenco_appuntamenti_vaccini:
-            for key in self.vaccino_per_tipologia:
-                if appuntamento.vaccino == key:
-                    self.vaccino_per_tipologia[appuntamento.vaccino] += 1
-
-        for appuntamento in self.elenco_appuntamenti_tamponi:
-            for key in self.tampone_per_tipologia:
-                if appuntamento.tipo_tampone == key:
-                    self.tampone_per_tipologia[appuntamento.tipo_tampone] += 1
 
 
 
--- statistiche/view/VistaStatisticheVaccini.py
+++ statistiche/view/VistaStatisticheVaccini.py
@@ -1,5 +1,8 @@
-from PyQt5.QtGui import QFont
+from PyQt5 import Qt
+from PyQt5.QtChart import QPieSeries, QChart, QChartView, QPieSlice
+from PyQt5.QtGui import QFont, QPen, QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QPushButton, QSizePolicy, QVBoxLayout, QHBoxLayout, QListView
+from PyQt5.QtCore import Qt
 
 from statistiche.controller.ControlloreStatistiche import ControlloreStatistiche
 
@@ -11,6 +14,8 @@ class VistaStatisticheVaccini(QWidget):
         super(VistaStatisticheVaccini, self).__init__(parent)
 
         self.controller = ControlloreStatistiche()
+
+        self.vaccino_per_tipologia = {"Astrazeneca": 0, "Moderna": 0, "Pfizer": 0}
 
         self.h_layout = QHBoxLayout()
         self.v_layout = QVBoxLayout()
@@ -29,17 +34,53 @@ class VistaStatisticheVaccini(QWidget):
         button_eff_collaterali.clicked.connect(self.go_eff_collaterali)
         self.h_layout.addWidget(button_eff_collaterali)
 
-        self.list_view = QListView()
         self.v_layout.addLayout(self.h_layout)
-        self.v_layout.addWidget(self.list_view)
+
 
         self.setLayout(self.v_layout)
         self.setWindowTitle("Statistiche Vaccini")
         self.resize(450, 350)
 
+        for appuntamento in self.controller.get_elenco_appuntamenti_vaccini():
+            for key in self.vaccino_per_tipologia:
+                if appuntamento.vaccino == key:
+                    self.vaccino_per_tipologia[appuntamento.vaccino] += 1
+
+
+
+
+
 
     def go_vaccini_somministrati(self):
-        pass
+        torta = QPieSeries()
+
+        for vaccino in self.vaccino_per_tipologia:
+            torta.append(vaccino, self.vaccino_per_tipologia[vaccino])
+
+        torta.setLabelsVisible()
+        torta.setLabelsPosition(QPieSlice.LabelInsideHorizontal)
+
+        colore = 50
+
+        for slice in torta.slices():
+            slice.setLabel("{:.1f}%".format(100 * slice.percentage()))
+            slice.setBrush(QColor(250,150,colore))
+            colore += 25
+
+        chart = QChart()
+        chart.addSeries(torta)
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+        chart.setTitle("Vaccini somministrati")
+        #chart.setTheme(QChart.ChartTheme)
+        chart.legend().setAlignment(Qt.AlignBottom)
+        chart.legend().markers(torta)[0].setLabel("Astrazeneca")
+        chart.legend().markers(torta)[1].setLabel("Moderna")
+        chart.legend().markers(torta)[2].setLabel("Pfizer")
+
+
+        chartview = QChartView(chart)
+        chartview.setRenderHint(QPainter.Antialiasing)
+        self.v_layout.addWidget(chartview)
 
 
     def go_eff_collaterali(self):
